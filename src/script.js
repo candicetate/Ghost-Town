ad3dbc95083fa2f6733789a99b17f372860b00d2
Candice Tate <67024645+candicetate@users.noreply.github.com>
2020-08-17 18:44:46 -0600

    added javascript animations

--- src/script.js
+++ src/script.js
@@ -10,3 +10,35 @@ let body = selectElement("body");
 menuToggler.addEventListener("click", function () {
   body.classList.toggle("open");
 });
+
+// Scroll animations
+
+window.sr = scrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
